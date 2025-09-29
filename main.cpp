1a2f20ffacbed6c972108d2f327612fdb4db8cbf
edixon-del <edixon@cub.uca.edu>
2025-09-29 11:54:06 -0500

    Project

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 /*
- TODO:(student) Add a comment header for the program here.
+Bearflix movie list
+implements the list processing the right output messages
+Ethen Dixon
  */
 
 #include <iostream>
@@ -7,18 +9,8 @@
 #include <string>
 #include <list>
 #include <algorithm>
-/**
- * @brief Searches for a given string value in a list of strings.
- *
- * This is a helper function that searches for a given string value
- * in a list of strings using the standard library find function.
- *
- * Interestingly, the standard list class does not have a find method.
- *
- * @param myList The list to search in.
- * @param value The value to search for.
- * @return true if the value is found, false otherwise.
- */
+#include <cstdlib>
+
 bool find(const std::list<std::string> &myList, const std::string &value)
 {
     bool found = (std::find(myList.begin(),
@@ -27,17 +19,71 @@ bool find(const std::list<std::string> &myList, const std::string &value)
     return found;
 }
 
-/**
- * @brief Reads, updates, and writes a list of movies.
- *
- * This function reads in a list of movies from a file,
- * adds new movies to the list, removes movies from the list,
- * and outputs the updated list to a file.
- *
- * @return int The exit status of the program.
- */
+static void stripCR(std::string& s) {
+    if (!s.empty() && s.back() == '\r') s.pop_back();
+}
+
 int main()
 {
-    // Follow instructions in project assignment
+    std::list<std::string> movies;
+    std::string line;
+
+    std::cout << "Reading in BearFlix movie list..." << std::endl;
+    std::ifstream in1("mymovies.txt");
+    if (!in1) {
+        std::cout << "File mymovies.txt cannot be opened." << std::endl;
+        return EXIT_FAILURE;
+    }
+    while (std::getline(in1, line)) {
+        stripCR(line);
+        if (!line.empty()) movies.push_back(line);
+    }
+    in1.close();
+
+    std::cout << "Adding new movies..." << std::endl;
+    std::ifstream in2("add_movies.txt");
+    if (!in2) {
+        std::cout << "File add_movies.txt cannot be opened." << std::endl;
+        return EXIT_FAILURE;
+    }
+    while (std::getline(in2, line)) {
+        stripCR(line);
+        if (line.empty()) continue;
+        if (find(movies, line)) {
+            std::cout << "Movie " << line << " cannot be added to the list." << std::endl;
+        } else {
+            movies.push_back(line);
+        }
+    }
+    in2.close();
+
+    std::cout << "Removing movies..." << std::endl;
+    std::ifstream in3("del_movies.txt");
+    if (!in3) {
+        std::cout << "File del_movies.txt cannot be opened." << std::endl;
+        return EXIT_FAILURE;
+    }
+    while (std::getline(in3, line)) {
+        stripCR(line);
+        if (line.empty()) continue;
+        if (!find(movies, line)) {
+            std::cout << "Movie " << line << " cannot be removed from the list." << std::endl;
+        } else {
+            movies.remove(line);
+        }
+    }
+    in3.close();
+
+    std::ofstream out("mymovies_updated.txt");
+    if (!out) {
+        std::cout << "File mymovies_updated.txt cannot be opened." << std::endl;
+        return EXIT_FAILURE;
+    }
+    for (const auto& title : movies) {
+        out << title << '\n';
+    }
+    out.close();
+
+    std::cout << "New movie list ready!" << std::endl;
     return EXIT_SUCCESS;
 }
